22217319b88bd931aa6a753244b995e07f5c7f57
Berk Altıok <berkpw@gmail.com>
2022-07-07 14:58:46 +0300

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import ReactDOM from "react-dom/client";
+import { MemoryRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./pages/App";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const root = ReactDOM.createRoot(
+  document.getElementById("app") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
