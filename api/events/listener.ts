e2baf5eba198feee5d9478bd6670ab7f03ec4e2e
Berk Altıok <berkpw@gmail.com>
2022-07-13 14:09:57 +0300

    feat: create settings method

--- api/events/listener.ts
+++ api/events/listener.ts
@@ -1,11 +1,11 @@
+import Settings from "../methods/settings"
 import Notify from "../methods/notify"
 import User from "../methods/user"
-import Settings from "../methods/settings"
 
 figma.ui.onmessage = (options: ListenerOptions) => {
   const { event } = options
 
-  if (event === "notify") Notify(options)
-  if (event === "user") User(options)
-  if (event === "settings") Settings(options)
+  if (event.startsWith("settings")) Settings(options)
+  if (event.startsWith("notify")) Notify(options)
+  if (event.startsWith("user")) User(options)
 }
--- api/methods/settings.ts
+++ api/methods/settings.ts
@@ -1,3 +1,42 @@
-const Settings = ({ data }: ListenerOptions) => {}
+interface Settings {
+  [key: string]: SettingsOptions | string
+}
+interface SettingsOptions {
+  value: string
+  type?: string
+}
+
+const setMultipleData = (settings: Settings) => {
+  Object.keys(settings).map((key) => {
+    figma.root.setPluginData(key, settings[key].toString())
+  })
+}
+const getMultipleData = () => {
+  let dataKeyList = figma.root.getPluginDataKeys()
+
+  let dataValueList = {}
+  dataKeyList.map((key) => {
+    ;(dataValueList as any)[key] = figma.root.getPluginData(key)
+  })
+
+  figma.ui.postMessage({
+    event: "settings/getMultipleData",
+    data: dataValueList,
+  })
+
+  return dataValueList
+}
+// const setData = ({}: SettingsOptions) => {}
+// const getData = (slug: string) => {}
+
+const Settings = ({ event, data }: ListenerOptions) => {
+  let runnerName = event.split("/")[1]
+  let runnerList: any = {
+    setMultipleData,
+    getMultipleData,
+  }
+
+  ;(runnerList[runnerName] as any)({ ...data })
+}
 
 export default Settings
