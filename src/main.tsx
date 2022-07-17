7e715464efa82440bbf7eeba892a8ee167ab8d62
Berk Altıok <berkpw@gmail.com>
2022-07-17 10:13:00 +0300

    feat: create custom scrollbar

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import ReactDOM from "react-dom/client"
 import Router from "@/router"
 import { StoreProvider } from "@/store"
 
+import SimpleBar from "simplebar-react"
+import "simplebar/dist/simplebar.min.css"
+
 import "@/styles/main.scss"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <StoreProvider>
-      <Router />
+      <SimpleBar>
+        <Router />
+      </SimpleBar>
     </StoreProvider>
   </React.StrictMode>
 )
