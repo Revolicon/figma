50e402279472d258d4f320a27f53d5a3604aaf4a
Berk Altıok <berkpw@gmail.com>
2022-07-31 09:33:47 +0300

    chore(Input): add input size

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,19 @@
 import React, { RefObject } from "react"
 import classNames from "classnames"
 
+type InputSize = "small" | "default"
+
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   [key: string]: any
   innerRef?: RefObject<any>
   className?: string
   center?: boolean
   full?: boolean
+  size?: any | InputSize
 }
 
 const Input: React.FC<Props> = (props) => {
-  const { innerRef, className, center, full } = props
+  const { innerRef, className, center, full, size = "default" } = props
 
   return (
     <input
@@ -18,6 +21,7 @@ const Input: React.FC<Props> = (props) => {
       ref={innerRef}
       className={classNames(
         "input",
+        [`input--size-${size}`],
         {
           "input--center": center,
           "input--full": full,
