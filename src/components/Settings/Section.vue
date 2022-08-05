048add9b58718a611e649a23488a56bea8336073
Berk Altıok <berkpw@gmail.com>
2022-08-06 01:00:47 +0300

    chore(Settings): create dynamic icon image

--- src/components/Settings/Section.vue
+++ src/components/Settings/Section.vue
@@ -6,7 +6,7 @@
     }"
   >
     <div class="section__header">
-      <div class="section__header-icon" v-html="icons['dark'][icon]" v-if="icons['dark'][icon]" />
+      <div class="section__header-icon" v-html="getIcon(icon)" v-if="getIcon(icon)" />
       <div class="section__header-title">{{ title }}</div>
       <div class="section__header-actions" v-if="slots.actions">
         <slot name="actions" />
@@ -21,44 +21,19 @@
 <script setup lang="ts">
   import { useSlots } from 'vue'
 
-  import DarkSize from '@/images/settings/sections/dark/size.svg?raw'
-  import DarkAppearance from '@/images/settings/sections/dark/appearance.svg?raw'
-  import DarkColor from '@/images/settings/sections/dark/color.svg?raw'
-  import DarkFinder from '@/images/settings/sections/dark/finder.svg?raw'
-  import DarkFrame from '@/images/settings/sections/dark/frame.svg?raw'
-  import DarkQuickActions from '@/images/settings/sections/dark/quickactions.svg?raw'
-
-  import LightSize from '@/images/settings/sections/light/size.svg?raw'
-  import LightAppearance from '@/images/settings/sections/light/appearance.svg?raw'
-  import LightColor from '@/images/settings/sections/light/color.svg?raw'
-  import LightFinder from '@/images/settings/sections/light/finder.svg?raw'
-  import LightFrame from '@/images/settings/sections/light/frame.svg?raw'
-  import LightQuickActions from '@/images/settings/sections/light/quickactions.svg?raw'
-
-  const icons: { [key: string]: any } = {
-    dark: {
-      size: DarkSize,
-      appearance: DarkAppearance,
-      color: DarkColor,
-      finder: DarkFinder,
-      frame: DarkFrame,
-      quickactions: DarkQuickActions,
-    },
-    light: {
-      size: LightSize,
-      appearance: LightAppearance,
-      color: LightColor,
-      finder: LightFinder,
-      frame: LightFrame,
-      quickactions: LightQuickActions,
-    },
-  }
-
   const slots = useSlots()
   const props = defineProps<{
-    icon: string
+    icon?: string
     title: string
   }>()
+
+  const getIcon: any = (icon: string) => {
+    return (
+      import.meta.globEager(`/src/images/settings/sections/**/*.svg`, {
+        as: 'raw',
+      }) as any
+    )[`/src/images/settings/sections/dark/${icon}.svg`]
+  }
 </script>
 
 <style scoped lang="scss">
