6760fbff3a7c8c51f6bee42b20f734b06710ea4c
Berk Altıok <berkpw@gmail.com>
2022-08-06 13:20:49 +0300

    chore(Settings): add logout button
    Add logout button other section
    
    Co-Authored-By: Berk Altıok <berk@berk.pw>

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -6,12 +6,23 @@
     <Section icon="finder" title="Finder"></Section>
     <Section icon="quickactions" title="Quick actions"></Section>
     <Section icon="appearance" title="Appearance"></Section>
+    <Section title="Other">
+      <Button variant="destructive" full @click="removeBetaKey">Logout</Button>
+    </Section>
   </SimpleBar>
 </template>
 
 <script setup>
   import { SimpleBar } from 'simplebar-vue3'
 
+  import { $post } from '@/utils/message'
+
+  import Button from '@/components/Button'
+
   import Section from '@/components/Settings/Section.vue'
   import Color from '@/components/Settings/Color'
+
+  const removeBetaKey = () => {
+    $post('settings/removeData', 'betaKey')
+  }
 </script>
