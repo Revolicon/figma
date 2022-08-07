179cdd815e9c5a88f160e3904260bbc3919bdac0
Berk Altıok <berkpw@gmail.com>
2022-08-07 09:09:36 +0300

    feat(ButtonIcon): create component

--- src/components/Button/Icon.vue
+++ src/components/Button/Icon.vue
@@ -1,9 +1,40 @@
 <template>
-  <div></div>
+  <component :is="ButtonTag" :href="href" :target="href && '_blank'" class="button">
+    <Icons :name="icon" size="16" />
+  </component>
 </template>
 
-<script>
-  export default {}
+<script setup>
+  import Icons from '@/components/Icons'
+
+  let props = defineProps({
+    icon: {
+      type: String,
+      required: true,
+    },
+    href: String,
+  })
+
+  const ButtonTag = props.href ? 'a' : 'button'
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+  .button {
+    color: var(--figma-color-text);
+    background: transparent;
+    height: 32px;
+    width: 32px;
+    border-radius: 4px;
+    border: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      color: var(--figma-color-text);
+    }
+    &:hover {
+      background: var(--figma-color-bg-secondary);
+    }
+  }
+</style>
